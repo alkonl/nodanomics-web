ea1353feb9799dfaef44c3dfd3a7999a579b56db
batonych <batonych@gmail.com>
2023-07-26 09:53:36 +0300

    refactor(password): changed password validation

--- src/utils/validation/index.ts
+++ src/utils/validation/index.ts
@@ -5,9 +5,8 @@ export const validation = {
     email: z.string().email(),
     password: z
         .string()
-        .min(2, 'password should have at least 2 alphabets')
-        .max(20, 'password should be no longer than 20 alphabets')
-        .refine((value) => /[a-zA-Z]/.test(value), 'password should contain only alphabets'),
+        .refine((value) => /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/
+            .test(value), 'Password at least 6 characters, 1 number and 1 special character (!@#$%^&*)'),
     // eslint-disable-next-line
     phoneNumber: z.string().refine((value) => /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/.test(value), 'Phone number is not valid. example +919367788755'),
     firstName:  z.string().min(3),
