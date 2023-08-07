2d99c46c8577c4142e98597eb323346a0f5543cd
batonych <batonych@gmail.com>
2023-08-07 16:37:18 +0300

    feat(edge animation): implemented base edge animation

--- src/component/DiagramEditor/CustomConnectionLine/DataConnection.tsx
+++ src/component/DiagramEditor/CustomConnectionLine/DataConnection.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
 // eslint-disable-next-line import/named
 import {BaseEdge, EdgeLabelRenderer, EdgeProps, getBezierPath} from 'reactflow';
-import {Box, Button} from "@mui/material";
 import {EElementType, IDataConnectionData} from "../../../interface";
 import {diagramEditorActions, useAppDispatch} from "../../../redux";
+import {Box, Button} from "@mui/material";
 import {EColor, EDGE_Z_INDEX} from "../../../constant";
+
+const CircleAnimation = () => {
+    return (
+        <circle cx="" cy="" r="8" fill="#529fd9">
+            <animateMotion dur="1.6s" repeatCount="indefinite">
+                <mpath xlinkHref="#wire"></mpath>
+            </animateMotion>
+        </circle>
+    )
+}
+
+function SimpleEdgeWithAnimation({edgePath}: { edgePath: string; }) {
+    return (
+        <g>
+            <path d={edgePath}
+                  stroke="#529fd9" strokeWidth="2" fill="none" id="wire"></path>
+            <circle cx="" cy="" r="8" fill="#529fd9">
+                <animateMotion dur="1.6s" repeatCount="indefinite">
+                    <mpath xlinkHref="#wire"></mpath>
+                </animateMotion>
+            </circle>
+        </g>
+    );
+}
 
 export const DataConnection: React.FC<EdgeProps<IDataConnectionData>> = (
     {
@@ -37,9 +61,25 @@ export const DataConnection: React.FC<EdgeProps<IDataConnectionData>> = (
             elementType: EElementType.Connection,
         }))
     }
+    const svg = 'M121.59 254L134.26 217.361H127.278L118.137 246.51H117.705L108.489 217.361H101.252L114.049 254H121.59Z'
+    console.log('edge data',
+        targetX,
+        targetY,
+        sourceX,
+        sourceY,
+        sourcePosition,
+        targetPosition,
+    )
+    console.log('edge style', style, edgePath)
+
+
+
+
     return (
         <>
-            <BaseEdge path={edgePath} markerEnd={markerEnd} style={style}/>
+            {/*<SimpleEdgeWithAnimation edgePath={edgePath}/>*/}
+            <CircleAnimation/>
+            <BaseEdge path={edgePath} markerEnd={markerEnd} style={style} id="wire"/>
             <EdgeLabelRenderer>
                 <Box
                     sx={{
