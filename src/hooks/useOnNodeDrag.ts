d7abdcdfebec6ad4e0aa2c0e2583ba0be39c66fd
batonych <batonych@gmail.com>
2023-08-23 15:24:33 +0300

    refactor(node resize): improve code structures of node resizing step 1.1

--- src/hooks/useOnNodeDrag.ts
+++ src/hooks/useOnNodeDrag.ts
@@ -3,75 +3,101 @@ import {IReactFlowNode, isINodeSize} from "../interface";
 import {diagramEditorActions, useAppDispatch, useDiagramEditorState} from "../redux";
 import {loopSize} from "../constant";
 import {useGetFlatChildrenNodes} from "./useGetFlatChildrenNodes";
+import {resizeParentOnDrag} from "../service";
 
 
 export const useOnNodeDrag = () => {
     const dispatch = useAppDispatch()
     const {diagramNodes} = useDiagramEditorState()
     const getChildrenNodes = useGetFlatChildrenNodes()
+
     return useCallback((event: ReactMouseEvent, node: IReactFlowNode) => {
-        if (!node.parentNode) return
-        const childrenNodes = getChildrenNodes({parentId: node.parentNode})
-
-
-        const parentNode = diagramNodes.find((diagramNode) => diagramNode.id === node.parentNode)
-        if (parentNode && isINodeSize(parentNode.data.style) && node.width && node.height) {
-            const parentSize = {
-                width: parentNode.data.style.width,
-                height: parentNode.data.style.height
-            }
-            const rightestChild = childrenNodes.reduce((prev, current) => {
-                if (prev.width !== null && current.width !== null && prev.width && current.width) {
-                    return (prev.position.x + prev.width > current.position.x + current.width) ? prev : current
-                }
-                return current
-            }, node)
-
-            const mostBottomChild = childrenNodes.reduce((prev, current) => {
-                const prevBottom = prev.position.y + (typeof prev.height === 'number' ? prev.height : 0)
-                const currentBottom = current.position.y + (typeof current.height === 'number' ? current.height : 0)
-                return prevBottom > currentBottom ? prev : current
-            }, node)
-
-            let updatedWidth: number | undefined
-            let updatedHeight: number | undefined
-
-            const isRightestChild = rightestChild?.id === node.id
-            const isMostBottomChild = mostBottomChild?.id === node.id
-
-            const preCalculatedWidth = parentNode.data.style.width + event.movementX
-            if (isRightestChild && preCalculatedWidth >= loopSize.minWidth && rightestChild.width) {
-                const widthBetween = parentSize.width - rightestChild.position.x - rightestChild.width
-
-                if (event.movementX < 0 && widthBetween > 20) {
-                    updatedWidth = preCalculatedWidth
-                } else if (widthBetween < 40) {
-                    const additionalIncrease = widthBetween < 20 ? 10 : 0
-                    updatedWidth = preCalculatedWidth + additionalIncrease
-                }
-
-            }
-
-            const preCalculatedHeight = parentNode.data.style.height + event.movementY
-            if (isMostBottomChild && preCalculatedHeight >= loopSize.minHeight && mostBottomChild.height) {
-                const heightBetween = parentSize.height - mostBottomChild.position.y - mostBottomChild.height
-                if (event.movementY < 0 && heightBetween > 20) {
-                    updatedHeight = preCalculatedHeight
-                } else if (heightBetween < 40) {
-                    const additionalIncrease = heightBetween < 20 ? 10 : 0
-                    updatedHeight = preCalculatedHeight + additionalIncrease
-                }
-            }
-
-            if (isRightestChild || isMostBottomChild) {
-                dispatch(diagramEditorActions.updateNodeSize({
-                    nodeId: parentNode.id, size: {
-                        width: updatedWidth || parentSize.width,
-                        height: updatedHeight || parentSize.height
-                    }
-                }))
-            }
+        const updateNodeSize = (params: {
+            nodeId: string,
+            size: { width: number, height: number }
+        }) => {
+            dispatch(diagramEditorActions.updateNodeSize(params))
         }
-
+        resizeParentOnDrag({diagramNodes, node, event, updateNodeSize})
     }, [dispatch, diagramNodes])
 }
+
+// It is probably deprecated. I am faced with  problems splitting this code into smaller functions
+// export const useDeprecatedOnNodeDrag = () => {
+//     const dispatch = useAppDispatch()
+//     const {diagramNodes} = useDiagramEditorState()
+//     const getChildrenNodes = useGetFlatChildrenNodes()
+//
+//     return useCallback((event: ReactMouseEvent, node: IReactFlowNode) => {
+//         const updateNodeSize = (params: {
+//             nodeId: string,
+//             size: { width: number, height: number }
+//         })=>{
+//             dispatch(diagramEditorActions.updateNodeSize(params))
+//         }
+//         // resizeParentOnDrag({diagramNodes, updateNodeSize, node, event})
+//         if (!node.parentNode) return
+//         const childrenNodes = getChildrenNodes({parentId: node.parentNode})
+//
+//
+//         const parentNode = diagramNodes.find((diagramNode) => diagramNode.id === node.parentNode)
+//         if (parentNode && isINodeSize(parentNode.data.style) && node.width && node.height) {
+//             const parentSize = {
+//                 width: parentNode.data.style.width,
+//                 height: parentNode.data.style.height
+//             }
+//             const rightestChild = childrenNodes.reduce((prev, current) => {
+//                 if (prev.width !== null && current.width !== null && prev.width && current.width) {
+//                     return (prev.position.x + prev.width > current.position.x + current.width) ? prev : current
+//                 }
+//                 return current
+//             }, node)
+//
+//             const mostBottomChild = childrenNodes.reduce((prev, current) => {
+//                 const prevBottom = prev.position.y + (typeof prev.height === 'number' ? prev.height : 0)
+//                 const currentBottom = current.position.y + (typeof current.height === 'number' ? current.height : 0)
+//                 return prevBottom > currentBottom ? prev : current
+//             }, node)
+//
+//             let updatedWidth: number | undefined
+//             let updatedHeight: number | undefined
+//
+//             const isRightestChild = rightestChild?.id === node.id
+//             const isMostBottomChild = mostBottomChild?.id === node.id
+//
+//             const preCalculatedWidth = parentNode.data.style.width + event.movementX
+//             if (isRightestChild && preCalculatedWidth >= loopSize.minWidth && rightestChild.width) {
+//                 const widthBetween = parentSize.width - rightestChild.position.x - rightestChild.width
+//
+//                 if (event.movementX < 0 && widthBetween > 20) {
+//                     updatedWidth = preCalculatedWidth
+//                 } else if (widthBetween < 40) {
+//                     const additionalIncrease = widthBetween < 20 ? 10 : 0
+//                     updatedWidth = preCalculatedWidth + additionalIncrease
+//                 }
+//
+//             }
+//
+//             const preCalculatedHeight = parentNode.data.style.height + event.movementY
+//             if (isMostBottomChild && preCalculatedHeight >= loopSize.minHeight && mostBottomChild.height) {
+//                 const heightBetween = parentSize.height - mostBottomChild.position.y - mostBottomChild.height
+//                 if (event.movementY < 0 && heightBetween > 20) {
+//                     updatedHeight = preCalculatedHeight
+//                 } else if (heightBetween < 40) {
+//                     const additionalIncrease = heightBetween < 20 ? 10 : 0
+//                     updatedHeight = preCalculatedHeight + additionalIncrease
+//                 }
+//             }
+//
+//             if (isRightestChild || isMostBottomChild) {
+//                 dispatch(diagramEditorActions.updateNodeSize({
+//                     nodeId: parentNode.id, size: {
+//                         width: updatedWidth || parentSize.width,
+//                         height: updatedHeight || parentSize.height
+//                     }
+//                 }))
+//             }
+//         }
+//
+//     }, [dispatch, diagramNodes])
+// }
---
+++ src/service/reactflow/layout/resizeParentOnDrag.ts
@@ -0,0 +1,92 @@
+import {MouseEvent} from "react";
+import {IReactFlowNode, isINodeSize} from "../../../interface";
+import {loopSize} from "../../../constant";
+
+export const rightestChild = (childrenNodes: IReactFlowNode[], node: IReactFlowNode) => {
+    return childrenNodes.reduce((prev, current) => {
+        if (prev.width !== null && current.width !== null && prev.width && current.width) {
+            return (prev.position.x + prev.width > current.position.x + current.width) ? prev : current
+        }
+        return current
+    }, node)
+}
+
+
+export const resizeParentOnDrag = (
+    {
+        diagramNodes,
+        updateNodeSize,
+        node,
+        event
+    }: {
+        event: MouseEvent,
+        node: IReactFlowNode,
+        diagramNodes: IReactFlowNode[]
+        updateNodeSize: (params: {
+            nodeId: string,
+            size: { width: number, height: number }
+        }) => void
+    }) => {
+    if (!node.parentNode) return
+    const childrenNodes = diagramNodes.filter((diagramNode) => diagramNode.parentNode === node.parentNode)
+    const parentNode = diagramNodes.find((diagramNode) => diagramNode.id === node.parentNode)
+
+    if (parentNode && isINodeSize(parentNode.data.style) && node.width && node.height) {
+        const parentSize = {
+            width: parentNode.data.style.width,
+            height: parentNode.data.style.height
+        }
+        const rightestChild = childrenNodes.reduce((prev, current) => {
+            if (prev.width !== null && current.width !== null && prev.width && current.width) {
+                return (prev.position.x + prev.width > current.position.x + current.width) ? prev : current
+            }
+            return current
+        }, node)
+
+        const mostBottomChild = childrenNodes.reduce((prev, current) => {
+            const prevBottom = prev.position.y + (typeof prev.height === 'number' ? prev.height : 0)
+            const currentBottom = current.position.y + (typeof current.height === 'number' ? current.height : 0)
+            return prevBottom > currentBottom ? prev : current
+        }, node)
+
+        let updatedWidth: number | undefined
+        let updatedHeight: number | undefined
+
+        const isRightestChild = rightestChild?.id === node.id
+        const isMostBottomChild = mostBottomChild?.id === node.id
+
+        const preCalculatedWidth = parentNode.data.style.width + event.movementX
+        if (isRightestChild && preCalculatedWidth >= loopSize.minWidth && rightestChild.width) {
+            const widthBetween = parentSize.width - rightestChild.position.x - rightestChild.width
+
+            if (event.movementX < 0 && widthBetween > 20) {
+                updatedWidth = preCalculatedWidth
+            } else if (widthBetween < 40) {
+                const additionalIncrease = widthBetween < 20 ? 10 : 0
+                updatedWidth = preCalculatedWidth + additionalIncrease
+            }
+
+        }
+
+        const preCalculatedHeight = parentNode.data.style.height + event.movementY
+        if (isMostBottomChild && preCalculatedHeight >= loopSize.minHeight && mostBottomChild.height) {
+            const heightBetween = parentSize.height - mostBottomChild.position.y - mostBottomChild.height
+            if (event.movementY < 0 && heightBetween > 20) {
+                updatedHeight = preCalculatedHeight
+            } else if (heightBetween < 40) {
+                const additionalIncrease = heightBetween < 20 ? 10 : 0
+                updatedHeight = preCalculatedHeight + additionalIncrease
+            }
+        }
+
+        if (isRightestChild || isMostBottomChild) {
+            updateNodeSize({
+                nodeId: parentNode.id,
+                size: {
+                    width: updatedWidth || parentNode.data.style.width,
+                    height: updatedHeight || parentNode.data.style.height
+                }
+            })
+        }
+    }
+}
