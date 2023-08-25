c8ce73c0d0bee9f380225ad096f006ddfb9e30ca
batonych <batonych@gmail.com>
2023-08-25 15:50:19 +0300

    feat(loop into loop): improved setParents on drop

--- src/hooks/useSetParentNode.ts
+++ src/hooks/useSetParentNode.ts
@@ -1,18 +1,57 @@
 import {useCallback} from "react";
 import {IReactFlowNode} from "../interface";
 import {diagramEditorActions, useAppDispatch} from "../redux";
-import {findParent} from "../service";
+import {findParent, IUpdateChildrenFunc, recursiveUpdateChildren} from "../service";
+
+const updateLoopChildren: IUpdateChildrenFunc = ({parentNode, node}) => {
+    return {
+        ...node,
+        zIndex: parentNode.zIndex ? parentNode.zIndex + 1 : 12,
+        data: {
+            ...node.data,
+            defaultZIndex: parentNode.data.defaultZIndex ? parentNode.data.defaultZIndex + 1 : 12,
+        }
+    }
+}
 
 export const useSetParentNode = () => {
     const dispatch = useAppDispatch()
-    const {updateNodeParent} = diagramEditorActions
-    return useCallback((node: IReactFlowNode, nodes: IReactFlowNode[]) => {
-            const parentNode = findParent(node, nodes)
-            if (parentNode) {
-                dispatch(updateNodeParent({
-                    node,
-                    parentNode
-                }))
+    const {bulkUpdateNodes} = diagramEditorActions
+    return useCallback((node: IReactFlowNode, diagramNodes: IReactFlowNode[]) => {
+        if (node.parentNode === undefined) {
+            const parentNode = findParent(node, diagramNodes)
+            if (parentNode && !parentNode.data.isCollapsed) {
+
+                const parentPosition = parentNode.positionAbsolute || parentNode.position
+                const childNode: IReactFlowNode = {
+                    ...node,
+                    data: {
+                        ...node.data,
+                        parentId: parentNode.id,
+                        defaultZIndex: parentNode.zIndex ? parentNode.zIndex + 1 : 11,
+                    },
+                    parentNode: parentNode.id,
+                    position: {
+                        x: node.position.x - parentPosition.x,
+                        y: node.position.y - parentPosition.y,
+                    },
+                    extent: 'parent',
+                    zIndex: parentNode.zIndex ? parentNode.zIndex + 1 : 11,
+                }
+
+
+                const updatedInnerChildren = recursiveUpdateChildren(diagramNodes, childNode, updateLoopChildren)
+
+                const nodesToUpdate = [childNode, ...updatedInnerChildren]
+                dispatch(bulkUpdateNodes(nodesToUpdate))
             }
-    }, [ dispatch])
+        }
+        // const parentNode = findParent(node, nodes)
+        // if (parentNode) {
+        //     dispatch(updateNodeParent({
+        //         node,
+        //         parentNode
+        //     }))
+        // }
+    }, [dispatch])
 }
