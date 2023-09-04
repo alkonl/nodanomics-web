327da92b807a8268d19b131949acbf350b61b951
batonych <batonych@gmail.com>
2023-09-04 10:49:23 +0300

    feat(multi select): implemented multi-select

---
+++ src/constant/keyCodes.ts
@@ -0,0 +1,10 @@
+export const keys = {
+    shift: 16,
+    ctrl: 17,
+    c: 67,
+    v: 86,
+    z: 90,
+    y: 89,
+}
+
+export const multiSelectKeyCodes = [keys.shift].map((key) => String(key))
--- src/hooks/useDiagramKeyboardManager.ts
+++ src/hooks/useDiagramKeyboardManager.ts
@@ -2,33 +2,34 @@ import {useUndoRedoDiagram} from "./useUndoRedoDiagram";
 import {useKeyPressDetector} from "./useKeyPressDetector";
 import {useEffect} from "react";
 import {useCopyPaste} from "./useCopyPaste";
+import {keys} from "../constant";
 
-const key = {
-    ctrl: 17,
-    c: 67,
-    v: 86,
-    z: 90,
-    y: 89,
-    shift: 16,
-}
+// const key = {
+//     ctrl: 17,
+//     c: 67,
+//     v: 86,
+//     z: 90,
+//     y: 89,
+//     shift: 16,
+// }
 
 export const useDiagramKeyboardManager = () => {
-    const pressedKeys = useKeyPressDetector(Object.values(key))
+    const pressedKeys = useKeyPressDetector(Object.values(keys))
 
     const {undoDiagram, redoDiagram} = useUndoRedoDiagram()
 
     const {copy, paste} = useCopyPaste()
     useEffect(() => {
-        if (pressedKeys[key.ctrl] && pressedKeys[key.z]) {
+        if (pressedKeys[keys.ctrl] && pressedKeys[keys.z]) {
             undoDiagram()
         }
-        if (pressedKeys[key.ctrl] && pressedKeys[key.y] || pressedKeys[key.ctrl] && pressedKeys[key.shift] && pressedKeys[key.z]) {
+        if (pressedKeys[keys.ctrl] && pressedKeys[keys.y] || pressedKeys[keys.ctrl] && pressedKeys[keys.shift] && pressedKeys[keys.z]) {
             redoDiagram()
         }
-        if (pressedKeys[key.ctrl] && pressedKeys[key.c]) {
+        if (pressedKeys[keys.ctrl] && pressedKeys[keys.c]) {
             copy()
         }
-        if (pressedKeys[key.ctrl] && pressedKeys[key.v]) {
+        if (pressedKeys[keys.ctrl] && pressedKeys[keys.v]) {
             paste()
         }
     }, [pressedKeys]);
