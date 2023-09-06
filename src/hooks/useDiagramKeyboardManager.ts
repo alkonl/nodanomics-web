46bc60ef357e473e5c6d41658e49b5be8b3be241
batonych <batonych@gmail.com>
2023-09-06 18:17:15 +0300

    fix(keyboard): fix keyboard

--- src/hooks/useDiagramKeyboardManager.ts
+++ src/hooks/useDiagramKeyboardManager.ts
@@ -1,32 +1,33 @@
 import {useKeyPress} from 'reactflow'
 import {useUndoRedoDiagram} from "./useUndoRedoDiagram";
-import {useKeyPressDetector} from "./useKeyPressDetector";
 import {useEffect} from "react";
 import {useCopyPaste} from "./useCopyPaste";
-import {keyCombination, keys} from "../constant";
-import {isKeyCombinationMatch} from "../utils";
 import {useDeleteSelectedNodes} from "./useDeleteSelectedNodes";
 
 export const useDiagramKeyboardManager = () => {
-    const {pressedKeyCodes} = useKeyPressDetector(Object.values(keys))
 
     const {undoDiagram, redoDiagram} = useUndoRedoDiagram()
 
     const {copy, paste} = useCopyPaste()
     const deleteNodes = useDeleteSelectedNodes()
-    const isNodeDeletePressed = useKeyPress(keyCombination.deleteReactFlow)
+
+    const isNodeDeletePressed = useKeyPress('Delete')
+    const isUndoPressed = useKeyPress(['ctrl + z', 'command + z'])
+    const isRedoPressed = useKeyPress(['ctrl + y', 'command + y', 'ctrl + c + z', 'command + c + z'])
+    const isCopyPressed = useKeyPress(['ctrl + c', 'command + c'])
+    const isPastePressed = useKeyPress(['ctrl + v', 'command + v'])
 
     useEffect(() => {
         if (isNodeDeletePressed) {
             deleteNodes()
+        } else if (isUndoPressed) {
+            undoDiagram()
+        } else if (isRedoPressed) {
+            redoDiagram()
+        } else if (isCopyPressed) {
+            copy()
+        } else if (isPastePressed) {
+            paste()
         }
-    }, [isNodeDeletePressed]);
-
-
-    useEffect(() => {
-        isKeyCombinationMatch(undoDiagram, keyCombination.undo, pressedKeyCodes)
-        isKeyCombinationMatch(redoDiagram, keyCombination.redo, pressedKeyCodes)
-        isKeyCombinationMatch(copy, keyCombination.copy, pressedKeyCodes)
-        isKeyCombinationMatch(paste, keyCombination.paste, pressedKeyCodes)
-    }, [pressedKeyCodes]);
+    }, [isNodeDeletePressed, isUndoPressed, isRedoPressed, isCopyPressed, isPastePressed]);
 }
