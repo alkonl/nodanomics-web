02f868d23a654d7ecfe0df45e2f893c6c9c21a54
batonych <batonych@gmail.com>
2023-09-12 11:39:29 +0300

    fix: websocket url problem

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,6 @@
 export const CONFIG = {
     API_URL: import.meta.env.VITE_API_URL,
-    API_WEBSOCKET_URL: import.meta.env.VITE_API_WEBSOCKET_URL,
+    API_SOCKET_URL: import.meta.env.VITE_API_SOCKET_URL,
     WEB_URL: import.meta.env.VITE_WEB_URL,
     IS_OFFLINE: import.meta.env.IS_OFFLINE || false,
 }
