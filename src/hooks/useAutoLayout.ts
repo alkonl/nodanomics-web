5474e344f3d8fee288f935cebf402a2e185e3937
batonych <batonych@gmail.com>
2023-09-18 18:06:32 +0300

    feat(auto layout): implemented draft autolayout v0.5

--- src/hooks/useAutoLayout.ts
+++ src/hooks/useAutoLayout.ts
@@ -2,6 +2,8 @@ import {IReactFlowEdge, IReactFlowNode} from "../interface";
 import Elk from "elkjs/lib/elk.bundled.js";
 import {diagramEditorActions, useAppDispatch, useDiagramEditorState} from "../redux";
 import lodash from "lodash";
+import {ElkExtendedEdge, ElkNode} from "elkjs/lib/elk-api";
+
 const elk = new Elk({
     // defaultLayoutOptions: {
     //     "elk.algorithm": "layered",
@@ -18,103 +20,125 @@ const elk = new Elk({
 const elkOptions = {
     'elk.algorithm': 'layered',
     'elk.layered.spacing.nodeNodeBetweenLayers': '100',
-    'elk.spacing.nodeNode': '80',
+    'elk.spacing.nodeNode': '100',
+    "elk.direction": "RIGHT",
+    // "elk.hierarchyHandling": "INCLUDE_CHILDREN",
 };
 const createGraphLayout = async (elements: {
     nodes: IReactFlowNode[];
     edges: IReactFlowEdge[];
 }, options = {}) => {
     const {nodes, edges} = elements;
-    const isHorizontal = true;
-    const graph = {
+
+    const childNodes: IReactFlowNode[] = []
+    const baseNodes: IReactFlowNode[] = []
+
+    nodes.forEach((node) => {
+        if (node.parentNode) {
+            childNodes.push(node)
+        } else {
+            baseNodes.push(node)
+        }
+    })
+
+    // const baseEdges: IReactFlowEdge[] = []
+    // const childEdges: IReactFlowEdge[] = []
+    // edges.forEach((edge) => {
+    //     // if(edge.s && edge.targetParentNode) {
+    //     //     childEdges.push(edge)
+    //     // } else {
+    //     //     baseEdges.push(edge)
+    //     // }
+    // })
+
+
+    const formatEdge = (edge: IReactFlowEdge) => {
+        return {
+            id: edge.id,
+            sources: [edge.source],
+            targets: [edge.target],
+        }
+    }
+
+    const getELKNode = (edges: IReactFlowEdge[], nodes: IReactFlowNode[], node: IReactFlowNode): ElkNode => {
+        const children = nodes.filter((n) => n.parentNode === node.id)
+        const elkNodeChildren = children.map((child) => getELKNode(edges, nodes, child))
+        // const relatedEdges = edges.filter((edge) => edge.source === node.id || edge.target === node.id)
+        // const elkNodeEdges = relatedEdges.map((edge) => formatEdge(edge))
+        return {
+            id: node.id,
+            width: typeof node.width === 'number' ? node.width : 300,
+            height: typeof node.height === 'number' ? node.height : 150,
+            children: elkNodeChildren,
+            // edges: elkNodeEdges,
+        }
+    }
+
+    const formattedNodes: ElkNode[] = nodes.map((node) => getELKNode(edges, nodes, node))
+
+    const edgesWithElkMetadata: ElkExtendedEdge[] = edges.map((edge) => ({
+        id: edge.id,
+        sources: [edge.source],
+        targets: [edge.target],
+    }))
+
+    const graph: ElkNode = {
         id: 'root',
         layoutOptions: options,
-        children: nodes.map((node) => ({
-            ...node,
-            // Adjust the target and source handle positions based on the layout
-            // direction.
-            targetPosition: isHorizontal ? 'left' : 'top',
-            sourcePosition: isHorizontal ? 'right' : 'bottom',
-
-            // Hardcode a width and height for elk to use when layouting.
-            width: 150,
-            height: 50,
-        })),
-        edges: edges,
+        children: formattedNodes,
+        edges: edgesWithElkMetadata,
     };
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    const layoutedGraph = await elk.layout(graph)
 
-    return {
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        nodes: layoutedGraph.children.map((node) => ({
+    const layoutedGraph = await elk.layout(graph, {
+        layoutOptions: elkOptions,
+
+    })
+
+    const getLayoutedNodesRecurseve = (node: ElkNode): ElkNode[] => {
+        const children = node.children?.map((child) => getLayoutedNodesRecurseve(child)).flat()
+        const layoutedNode = {
+            id: node.id,
+            position: {
+                x: node.x,
+                y: node.y,
+            },
+            width: node.width,
+            height: node.height,
+        }
+        return [layoutedNode, ...(children || [])]
+    }
+    const layoutedNodes = layoutedGraph.children?.map((child) => getLayoutedNodesRecurseve(child)).flat()
+    const nodesWithUpdatedPosition = elements.nodes?.map((node) => {
+        const layoutedNode = layoutedGraph.children?.find((n) => n.id === node.id)
+        return {
             ...node,
-            // React Flow expects a position property on the node instead of `x`
-            // and `y` fields.
-            position: {x: node.x, y: node.y},
-        })),
+            position: {
+                x: layoutedNode?.x || node.position?.x,
+                y: layoutedNode?.y || node.position?.y,
+            }
+        }
+    })
+    return {
+
+        nodes: nodesWithUpdatedPosition,
 
         edges: layoutedGraph.edges,
     }
 
-    // const nodes: ElkNode[] = [];
-    // const edges: ElkExtendedEdge[] = [];
-    //
-    // elements.nodes.forEach((el) => {
-    //     if (isNode(el)) {
-    //         nodes.push({
-    //             width: el?.__rf?.width,
-    //             height: el?.__rf?.height,
-    //             id: el.id,
-    //         });
-    //     }
-    // });
-    // elements.edges.forEach((el) => {
-    //     edges.push({
-    //         id: el.id,
-    //         source: el.source,
-    //         target: el.target,
-    //     });
-    // })
-    // console.log('before: elk.layout')
-    // const newGraph = await elk.layout(
-    //     {
-    //         id: "root",
-    //         children: nodes,
-    //         edges: edges
-    //     }
-    // );
-    // console.log('after: elk.layout')
-    //
-    // const structuredNodes = elements.nodes.map((el) => {
-    //     if (isNode(el)) {
-    //         const node = newGraph?.children?.find((n) => n.id === el.id);
-    //         if (node?.x && node?.y && node?.width && node?.height) {
-    //             el.position = {
-    //                 x: node.x - node.width / 2 + Math.random() / 1000,
-    //                 y: node.y - node.height / 2
-    //             };
-    //         }
-    //     }
-    //     return el;
-    // });
-    // return {
-    //     nodes: structuredNodes,
-    //     edges: elements.edges
-    // }
+
 };
 
 export const useAutoLayout = () => {
     const {diagramNodes, diagramEdges} = useDiagramEditorState()
     const dispatch = useAppDispatch()
     return async () => {
+        console.log('useAutoLayout:', diagramNodes)
 
         const structured = await createGraphLayout({
             nodes: lodash.cloneDeep(diagramNodes),
             edges: lodash.cloneDeep(diagramEdges),
         })
-        // dispatch(diagramEditorActions.bulkUpdateNodes(structured.nodes))
+        console.log('structured:', structured)
+        dispatch(diagramEditorActions.bulkUpdateNodes(structured.nodes))
     }
 }
