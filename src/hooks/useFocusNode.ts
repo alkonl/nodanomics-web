c0ed5fff6152a84415f5e7b7766b8de7ff4c6abd
batonych <batonych@gmail.com>
2023-09-19 19:07:02 +0300

    feat(Node Hierarchy): improved focusing when parent is collapsed

--- src/hooks/useFocusNode.ts
+++ src/hooks/useFocusNode.ts
@@ -1,20 +1,35 @@
 import {IReactFlowNode} from "../interface";
 import {useReactFlowInstance} from "./useReactFlowInstance";
+import {diagramEditorActions, useAppDispatch, useDiagramEditorState} from "../redux";
+import {findAllParents} from "../service";
 
 export const useFocusNode = () => {
+    const {diagramNodes} = useDiagramEditorState()
     const {reactFlowInstance} = useReactFlowInstance().data;
-    return (node: IReactFlowNode) => {
+    const dispatch = useAppDispatch();
+
+    return (selectedNodeToFocus: IReactFlowNode) => {
+
+        const parents = findAllParents(selectedNodeToFocus, diagramNodes)
+        const collapsedParents = parents.reverse().find(p => p.data.isCollapsed)
+        const nodeToFocus = collapsedParents || selectedNodeToFocus
+
         const nodeSize = {
-            width: node.width || 250,
-            height: node.height || 100,
+            width: nodeToFocus.width || 250,
+            height: nodeToFocus.height || 100,
         }
         const nodePosition = {
-            x:  node.positionAbsolute?.x || node.position.x,
-            y: node.positionAbsolute?.y || node.position.y,
+            x:  nodeToFocus.positionAbsolute?.x || nodeToFocus.position.x,
+            y: nodeToFocus.positionAbsolute?.y || nodeToFocus.position.y,
         }
         const x = nodePosition.x + nodeSize.width / 2;
         const y = nodePosition.y + nodeSize.height / 2;
         const zoom = 1.85;
+
         reactFlowInstance?.setCenter(x, y, {zoom, duration: 600});
+        dispatch(diagramEditorActions.setEditElement({
+            id: selectedNodeToFocus.id,
+            elementType: selectedNodeToFocus.data.elementType,
+        }));
     };
 }
