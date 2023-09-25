75525ced75edce43809d1575ab6c8a4b06272f5d
batonych <batonych@gmail.com>
2023-09-25 10:53:51 +0300

    feat(layers): implemented auto selecting

--- src/hooks/useDiagramSettings.ts
+++ src/hooks/useDiagramSettings.ts
@@ -1,6 +1,7 @@
 import {diagramEditorActions, useAppDispatch, useDiagramEditorState} from "../redux";
 import {useGetDiagramSettingsQuery} from "../api";
 import {useEffect} from "react";
+import {IDiagramLayer} from "../interface";
 
 export const useDiagramSettings = () => {
     const {currentDiagramId} = useDiagramEditorState()
@@ -11,8 +12,18 @@ export const useDiagramSettings = () => {
 
     useEffect(() => {
         if (diagramSettings?.DiagramLayers) {
-            dispatch(diagramEditorActions.setDiagramLayers(diagramSettings.DiagramLayers))
+            const isLayerSelected = diagramSettings.DiagramLayers.some(layer => layer.isSelected)
+            let preparedLayers: IDiagramLayer[] = diagramSettings.DiagramLayers
+            if(!isLayerSelected && diagramSettings.DiagramLayers[0]){
+                const selectedLayer: IDiagramLayer = {
+                    ...diagramSettings.DiagramLayers[0],
+                    isSelected: true,
+                }
+                preparedLayers = [selectedLayer, ...diagramSettings.DiagramLayers.slice(1)]
+            }
+
+            dispatch(diagramEditorActions.setDiagramLayers(preparedLayers))
 
         }
-    }, [diagramSettings,dispatch]);
+    }, [diagramSettings,dispatch, diagramSettings]);
 }
