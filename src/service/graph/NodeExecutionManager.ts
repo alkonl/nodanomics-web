89ff1415609fbf3e033780844a75f0f2ec67d218
batonych <batonych@gmail.com>
2023-09-27 16:01:36 +0300

    feat(run manager): implemented that second node will only execute on second step

--- src/service/graph/NodeExecutionManager.ts
+++ src/service/graph/NodeExecutionManager.ts
@@ -6,16 +6,18 @@ export class NodeExecutionManager {
     next: IChainItem[] = []
     runManager: RunManager
     reason?: string
+    countOfExecuted = 0
 
     constructor(runManager: RunManager) {
         this.runManager = runManager
     }
 
     invokeNodesToExecute() {
-        if (this.executionCount === 0) {
+        if (this.executionCount === 0 && this.countOfExecuted <= this.runManager.currentStep) {
             this.current = [...this.next]
             this.executionCount = this.next.length
             this.next = []
+            this.countOfExecuted++
 
             for (const argument of this.current) {
                 this.executionCount--
