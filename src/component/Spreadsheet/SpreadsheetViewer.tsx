bd7b7ca4e56fbfad398bec43de18704a61370b3c
batonych <batonych@gmail.com>
2023-10-17 17:58:01 +0300

    feat(dataset recording): implemented showing table with new cells

--- src/component/Spreadsheet/SpreadsheetViewer.tsx
+++ src/component/Spreadsheet/SpreadsheetViewer.tsx
@@ -4,9 +4,9 @@ import {useGetSpreadSheetQuery, useUpdateSpreadsheetMutation, useUseDeleteSpread
 import {MButton} from "../base";
 import {EColor, EFontColor} from "../../constant";
 import {useDiagramEditorState} from "../../redux";
-import {ISpreadsheetRowsData, IStructuredSpreadsheetData} from "../../interface";
+import {IStructuredSpreadsheetData} from "../../interface";
 import lodash from "lodash";
-import {ISpreadsheetView} from "../../interface/busines/spreadsheet/spreadsheetView";
+import {isISpreadsheetExistedValueView, ISpreadsheetView} from "../../interface/busines/spreadsheet/spreadsheetView";
 
 const getValueFromDataset = ({dataset, x, y}: {
     dataset: IStructuredSpreadsheetData,
@@ -29,9 +29,10 @@ export const SpreadsheetViewer: React.FC<{
     const mappedSpreadSheet = useMemo(() => {
         if (!data) return undefined
         const mappedSpreadSheet: ISpreadsheetView = lodash.cloneDeep(data)
+        const datasetData = spreadsheets?.[spreadsheetId]
+
         for (let i = 0; i < data.rows.length; i++) {
             for (let j = 0; j < data.rows[i].values.length; j++) {
-                const datasetData = spreadsheets?.[spreadsheetId]
                 let cellContent: string = data.rows[i].values[j].content
                 let isValueFromDataset = false
                 if (datasetData) {
@@ -53,8 +54,30 @@ export const SpreadsheetViewer: React.FC<{
                     }
 
                 }
-                mappedSpreadSheet.rows[i].values[j].content = cellContent
-                mappedSpreadSheet.rows[i].values[j].isChanged = isValueFromDataset
+                const cell = mappedSpreadSheet.rows[i].values[j]
+                cell.content = cellContent
+                if (isISpreadsheetExistedValueView(cell)) {
+                    cell.isChanged = isValueFromDataset
+                }
+            }
+        }
+        const datasetDataRowLength = datasetData?.rows.length || 0
+        const updatedLength = datasetDataRowLength - mappedSpreadSheet.rows.length
+        if (updatedLength > 0 && datasetData) {
+
+            for (let i = mappedSpreadSheet.rows.length; i < datasetData.rows.length; i++) {
+                const formattedNewDatasetValues = datasetData.rows[i].map((content) => ({
+                    content: content.toString(),
+                }))
+                // fill xAxis cells
+                const fillXAxisCells = Array.from({length: datasetData.xAxisIndex + 1}, () => ({
+                    content: '',
+                }))
+                const formattedValues = [...fillXAxisCells, ...formattedNewDatasetValues]
+                mappedSpreadSheet.rows.push({
+                    sheetId: spreadsheetId,
+                    values: formattedValues,
+                })
             }
         }
         return mappedSpreadSheet
@@ -63,7 +86,7 @@ export const SpreadsheetViewer: React.FC<{
 
     const formattedTable = useMemo(() => {
         if (!mappedSpreadSheet) return undefined
-
+        console.log('mappedSpreadSheet', mappedSpreadSheet)
         // replace empty cells with
         const formattedRows = []
         for (let i = 0; i < mappedSpreadSheet.rows.length; i++) {
@@ -81,7 +104,7 @@ export const SpreadsheetViewer: React.FC<{
                 if (skipValues && j >= skipValues.from && j <= skipValues.to) {
                     continue
                 }
-                const colspan = defaultCell.merge ? (defaultCell.merge.e.c - defaultCell.merge.s.c + 1) : undefined
+                const colspan = (isISpreadsheetExistedValueView(defaultCell) && defaultCell.merge) ? (defaultCell.merge.e.c - defaultCell.merge.s.c + 1) : undefined
                 if (colspan && colspan > 1) {
                     skipValues = {
                         from: j,
@@ -126,7 +149,7 @@ export const SpreadsheetViewer: React.FC<{
             }[] = []
             mappedSpreadSheet.rows.forEach((row) => {
                 return row.values.forEach((cell) => {
-                    if (cell.isChanged) {
+                    if (isISpreadsheetExistedValueView(cell) && cell.isChanged) {
                         toUpdateValues.push({
                             id: cell.id,
                             content: cell.content.toString(),
@@ -188,14 +211,14 @@ export const SpreadsheetViewer: React.FC<{
                         color: EFontColor.white,
                     }} aria-label="simple table">
                         <TableBody>
-                            {formattedTable.rows.map((row) => (
+                            {formattedTable.rows.map((row, i) => (
                                 <TableRow
-                                    key={row.id}
+                                    key={i}
                                 >
-                                    {row.values.map((cell) => (
+                                    {row.values.map((cell, j) => (
                                         <TableCell
                                             colSpan={cell.colspan}
-                                            key={cell.content}
+                                            key={`${cell.content}_${j}`}
                                             sx={{border: 1}}
                                             align="left"
                                         >{cell.content}</TableCell>
--- src/interface/busines/spreadsheet/spreadsheetView.ts
+++ src/interface/busines/spreadsheet/spreadsheetView.ts
@@ -1,27 +1,52 @@
+export interface ISpreadsheetNewValueView {
+    content: string;
+}
+
+export interface ISpreadsheetExistedValueView {
+    content: string;
+    id: string;
+    rowId: string;
+    merge?: {
+        s: {
+            c: number;
+            r: number;
+        };
+        e: {
+            c: number;
+            r: number;
+        };
+    }
+    columnId: string | null;
+    isChanged?: boolean;
+}
+
+export const isISpreadsheetExistedValueView = (value: ISpreadsheetValueView): value is ISpreadsheetExistedValueView => {
+    return (value as ISpreadsheetExistedValueView).id !== undefined;
+}
+
+export const isISpreadsheetNewValueView = (value: ISpreadsheetValueView): value is ISpreadsheetNewValueView => {
+    return (value as ISpreadsheetNewValueView).content !== undefined;
+}
+
+export type ISpreadsheetValueView = ISpreadsheetNewValueView | ISpreadsheetExistedValueView;
+
+export interface ISpreadsheetExistedRowView {
+    id: string;
+    sheetId: string;
+    values: ISpreadsheetValueView[];
+}
+
+export interface ISpreadsheetNewRowView {
+    sheetId: string;
+    values: ISpreadsheetValueView[];
+}
+
+export type  ISpreadsheetRowView = ISpreadsheetExistedRowView | ISpreadsheetNewRowView
+
 export interface ISpreadsheetView {
     createdAt: string;
     id: string;
     name: string;
     projectId: string;
-    rows: {
-        id: string;
-        sheetId: string;
-        values: {
-            content: string;
-            id: string;
-            rowId: string;
-            merge?: {
-                s: {
-                    c: number;
-                    r: number;
-                };
-                e: {
-                    c: number;
-                    r: number;
-                };
-            }
-            columnId: string | null;
-            isChanged?: boolean;
-        }[];
-    }[]
+    rows: ISpreadsheetRowView[]
 }
